6c4caa1804071ef8a0e42e4ba6ae67d5140969af
Dec Marse <145652280+dmarxie@users.noreply.github.com>
2025-06-13 12:44:55 +0800

    fix(models): update default role field to use string instead of enum in user creation

--- backend/api/models.py
+++ backend/api/models.py
@@ -31,7 +31,7 @@ class UserManager(BaseUserManager):
         """Create and save a SuperUser with the given email and password."""
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('role', User.ADMIN)
+        extra_fields.setdefault('role', 'ADMIN')
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
