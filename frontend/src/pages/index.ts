3f267fe40a81ff9a433ae64a6856d2fa60fef962
Dec Marse <145652280+dmarxie@users.noreply.github.com>
2025-06-15 03:09:48 +0800

    feat(frontend, admin): add basic admin login page and dashboard component

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,3 +1,6 @@
+export { AdminDashboard } from './AdminDashboard';
 export { AdminLoginPage } from './AdminLoginPage';
+export { Dashboard } from './Dashboard';
 export { HomePage } from './HomePage';
 export { LoginPage } from './LoginPage';
+export { Register } from './Register';
