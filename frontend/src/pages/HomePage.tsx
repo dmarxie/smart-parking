a362299a323f8e252f8d56810ddc9d958aa4089d
Dec Marse <145652280+dmarxie@users.noreply.github.com>
2025-06-14 19:23:20 +0800

    feat(frontend): add homepage

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,37 @@
+import { Button, Col, Container, Image } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import homepageImage from '../assets/images/homepage-bg.png';
+
+/**
+ * Homepage component
+ * @returns Homepage component
+ */
+export function HomePage() {
+  const navigate = useNavigate();
+
+  return (
+    <Container className="homepage px-0">
+      <Image src={homepageImage} alt="Car Image" className="homepage-image" />
+      <Container className="p-4 h-100 d-flex flex-column">
+        <h1 className="welcome-title mb-2">Welcome to Navipark</h1>
+        <p className="welcome-subtitle lead mb-5">Your hassle-free parking solution starts here.</p>
+
+        <Container className="homepage--login-options-container px-0 pt-5">
+          <h3 className="text-white">Who's logging in?</h3>
+          <Col className="w-100 d-flex flex-column gap-3">
+            <Button size="lg" className="btn-primary w-100 py-3" onClick={() => navigate('/login')}>
+              User Login
+            </Button>
+            <Button
+              size="lg"
+              className="btn-secondary w-100 py-3"
+              onClick={() => navigate('/admin/login')}
+            >
+              Admin Login
+            </Button>
+          </Col>
+        </Container>
+      </Container>
+    </Container>
+  );
+}
---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,3 @@
+export { AdminLoginPage } from './AdminLoginPage';
+export { HomePage } from './HomePage';
+export { LoginPage } from './LoginPage';
